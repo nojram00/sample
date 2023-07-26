75d06573458ce7078eedfc0722e763f8a717c253
hersvin13 <87289544+hersvin13@users.noreply.github.com>
2023-07-26 14:00:38 +0800

    Update changeDevicePassword.php

--- changeDevicePassword.php
+++ changeDevicePassword.php
@@ -1,76 +1,8 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Change Password</title>
-    <style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 20px;
-    }
-
-    h2 {
-        margin-bottom: 20px;
-    }
-
-    label {
-        display: block;
-        margin-bottom: 5px;
-    }
-
-    input {
-        padding: 5px;
-        margin-bottom: 10px;
-        width: 200px;
-    }
-
-    #send {
-        background-color: greenyellow;
-        width: 210px;
-    }
-
-    #postButton {
-        padding: 10px 20px;
-        background-color: #4CAF50;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-
-    #postButton:hover {
-        background-color: #45a049;
-    }
-
-    #responseDiv {
-        margin-top: 20px;
-        border: 1px solid #ccc;
-        padding: 10px;
-        border-radius: 5px;
-    }
-
-    .error {
-        color: red;
-    }
-    </style>
-</head>
-
-<body>
-    <h2>Change Password</h2>
-    <form method="post" action="">
-        <label for="oldPassword">Old Password:</label>
-        <input type="password" name="oldPassword" id="oldPassword" required><br><br>
-
-        <label for="newPassword">New Password:</label>
-        <input type="password" name="newPassword" id="newPassword" required><br><br>
-
-        <input type="submit" name="submit" value="Change Password">
-    </form>
-    <?php
+<?php
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Set the API endpoint
-    $apiEndpoint = "http://192.168.0.115:8090/setPassWord";
+    $apiEndpoint = "http://192.168.1.59:8090/setPassWord";
 
     // Retrieve old and new passwords from the form
     $oldPassword = $_POST["oldPassword"];
@@ -100,34 +32,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $response = curl_exec($curl);
 
     // Check for cURL errors
-        if (curl_errno($curl)) {
-            echo "Error: " . curl_error($curl);
+         if (curl_errno($curl)) {
+        echo "Error: " . curl_error($curl);
+    } else {
+        // Process the API response (you may want to handle success/error messages accordingly)
+        $decodedResponse = json_decode($response, true);
+        if ($decodedResponse["success"]) {
+            echo "<script>alert('Password changed successfully!');  window.history.go(-2);</script>";
         } else {
-            // Process the API response (you may want to handle success/error messages accordingly)
-            $decodedResponse = json_decode($response, true);
-            if ($decodedResponse["success"]) {
-                echo "Password changed successfully!";
-            } else {
-                echo "" . $decodedResponse["msg"];
-            }
+            echo "<script>alert('" . $decodedResponse["msg"] . "'); window.history.go(-1);</script>";
         }
-        
+    }
         // Close cURL session
         curl_close($curl);
     }
 
     
 
-    // // Output the response
-    // if ($response === "SUCCESS") {
-    //     echo "Password changed successfully!";
-    // } elseif ($response === "WRONG_OLD_PASSWORD") {
-    //     echo "Wrong old password. Password not changed.";
-    // } else {
-    //     echo "An error occurred while changing the password.";
-    // }
-    // }
 ?>
-</body>
-
-</html>
